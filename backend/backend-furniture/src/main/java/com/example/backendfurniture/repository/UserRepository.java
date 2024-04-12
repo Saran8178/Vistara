08778a7f6ff15a43001efed3b6aae6230841852d
Saran8178 <152619892+Saran8178@users.noreply.github.com>
2024-04-12 14:23:32 +0530

    Backend_Version_2.0

--- backend/backend-furniture/src/main/java/com/example/backendfurniture/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backendfurniture.repository;
-
-import com.example.backendfurniture.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User,Long>{
-
-}
--- backend/backend/backend/src/test/java/com/freshnco/backend/BackendApplicationTests.java
+++ backend/backend/backend/src/test/java/com/freshnco/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backendfurniture;
+package com.freshnco.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendFurnitureApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
